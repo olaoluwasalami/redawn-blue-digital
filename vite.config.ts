4a437f3b7840774ae62eed42c3d7a8beb96bc65a
Olaoluwa Ademola Salami <olaoluwaasalami@gmail.com>
2025-09-25 19:56:15 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/redawn-blue-digital/' // Replace with your GitHub repo name
+  base: '/' // Replace with your GitHub repo name
 }));
